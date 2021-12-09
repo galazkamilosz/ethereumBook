27604dd14bc173f832f458f34f179e854e126928
Milosz Galazka <m.galazkamilosz@gmail.com>
2021-12-10 00:46:21 +0100

    first chapters

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math"
+	"math/big"
+	"os"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	file, err := os.Open("config.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client, err := ethclient.Dial(string(data))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("connected!")
+	_ = client
+
+	address := common.HexToAddress("0x71c7656ec7ab88b098defb751b7401b5f6d8976f")
+	balance, err := client.BalanceAt(context.Background(), address, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balance)
+
+	fbalance := new(big.Float)
+	fbalance.SetString(balance.String())
+	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
+	fmt.Println(ethValue)
+
+	pendingBalance, err := client.PendingBalanceAt(context.Background(), address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(pendingBalance)
+
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKeyBytes := crypto.FromECDSA(privateKey)
+	fmt.Println(hexutil.Encode(privateKeyBytes)[2:])
+
+	publicKey := privateKey.Public()
+	fmt.Println(publicKey)
+}
